da422f530ce6de2daf269ee346e196ad1b427b2c
Robert Gerth <robert.gerth@mail.de>
2021-09-24 17:26:37 +0200

    adds regression models, adds main arguments

--- src/create_model/create_model.py
+++ src/create_model/create_model.py
@@ -1,19 +1,21 @@
 
 # Load libraries
 import numpy as np
-import shap
 import pandas as pd
+import shap
 from matplotlib import pyplot
 from pandas import read_csv
 from pandas.plotting import scatter_matrix
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.ensemble import (GradientBoostingClassifier,
                               RandomForestClassifier, RandomForestRegressor)
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LinearRegression, LogisticRegression
 from sklearn.metrics import (accuracy_score, classification_report,
-                             confusion_matrix, f1_score, roc_auc_score)
-from sklearn.model_selection import (StratifiedKFold, cross_val_score,
-                                     train_test_split)
+                             confusion_matrix, explained_variance_score,
+                             f1_score, max_error, mean_absolute_error,
+                             mean_squared_error, r2_score, roc_auc_score)
+from sklearn.model_selection import (StratifiedKFold, cross_val_predict,
+                                     cross_val_score, train_test_split)
 from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import (LabelEncoder, MinMaxScaler, OneHotEncoder,
@@ -22,84 +24,190 @@ from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 
 
-def create_model(data, target):
+def create_model(data, target, bool_classification=False):
+    """
+    creates, validates and explains model
+    """
+    # creates training and testdata
+    X_train, Y_train, X_validation, Y_validation, columns_X = prepare_model_dataset(
+        data=data, target=target, bool_classification=bool_classification)
 
+    if bool_classification:
+        models = []
+        # models.append(('LR', LogisticRegression(solver='liblinear', multi_class='ovr')))
+        # models.append(('LDA', LinearDiscriminantAnalysis()))
+        # models.append(('KNN', KNeighborsClassifier()))
+        # models.append(('CART', DecisionTreeClassifier()))
+        # models.append(('NB', GaussianNB()))
+        # models.append(('SVM', SVC(gamma='auto')))
+        models.append(('RF', RandomForestClassifier()))
+        models.append(('GBC', GradientBoostingClassifier()))
+        # models.append(('RFR',RandomForestRegressor()))
+
+        # evaluate each model in turn
+        results = []
+        names = []
+        for name, model in models:
+            print('-'*100)
+            # run classification model
+
+            model = run_model_classifier(model=model, X_train=X_train, Y_train=Y_train, X_validation=X_validation,
+                                         Y_validation=Y_validation, results=results, name=name, names=names)
+        print('-'*100)
+
+    else:
+        models = []
+        models.append(('LR', LinearRegression()))
+        models.append(('RFR', RandomForestRegressor()))
+        # evaluate each model in turn
+        results = []
+        names = []
+        for name, model in models:
+            print('-'*100)
+
+            # run regression model
+            model = run_model_regression(model=model, X_train=X_train, Y_train=Y_train, X_validation=X_validation,
+                                         Y_validation=Y_validation, results=results, name=name, names=names)
+        print('-'*100)
+
+    # Explains Tree Model
+    try:
+        explain_tree_model(model=model, X_validation=X_validation,
+                      columns_X=columns_X)
+    except:
+        print('Kein Baum-Modell')
+        pass
+    print('End')
+
+
+def prepare_model_dataset(data, target, bool_classification=True):
+    """
+    creates an model dataset
+    """
+    # drop non-numeric data
     data = data[data.columns[(data.dtypes != 'object')]]
+    
+    # drop choosen columns
     data = data.drop(['ID', 'OBJECTID', 'SECOND', 'DAY', 'MONTH', 'HOUR', 'MINUTE', 'REGION_CODE',
                      'YEAR_delete', 'COUNTRY_delete', 'LATITUDE', 'LONGITUDE', 'YEAR'], axis=1, errors='ignore')
+    
+    # handle missing data
     #data = data.fillna(data.mean())
     data = data.fillna(0)
+
+    # save names
     X = data.drop(target, axis=1)
+    columns_X = X.columns
+    
+    # set target value
+    if bool_classification:
+        y = data[target] > 0
+    else:
+        y = data[target]
 
-    # TODO Target
-    y = data[target] > 0
+    # divide data in test and train and X und y
     X_train, X_validation, Y_train, Y_validation = train_test_split(
-        X, y, test_size=0.20, random_state=1, stratify=y)
+        X, y, test_size=0.20, random_state=1)
 
+    # print partition
     print('Train: \n', 'True:', Y_train.sum(), '\n',
           'False:', len(Y_train)-Y_train.sum(), '\n')
     print('Test: \n', 'True:', Y_validation.sum(), '\n',
           'False:', len(Y_validation)-Y_validation.sum(), '\n')
 
+    # standard data
     scaler = StandardScaler()
     X_train = scaler.fit_transform(X_train)
     X_validation = scaler.transform(X_validation)
 
+    # calibrate data
     scaler = MinMaxScaler()
     X_train = scaler.fit_transform(X_train)
     X_validation = scaler.transform(X_validation)
+    return X_train, Y_train, X_validation, Y_validation, columns_X
 
-    models = []
-    # models.append(('LR', LogisticRegression(solver='liblinear', multi_class='ovr')))
-    # models.append(('LDA', LinearDiscriminantAnalysis()))
-    # models.append(('KNN', KNeighborsClassifier()))
-    # models.append(('CART', DecisionTreeClassifier()))
-    # models.append(('NB', GaussianNB()))
-    # models.append(('SVM', SVC(gamma='auto')))
-    models.append(('RF', RandomForestClassifier()))
-    models.append(('GBC', GradientBoostingClassifier()))
-    # models.append(('RFR',RandomForestRegressor()))
 
-    # evaluate each model in turn
-    results = []
-    names = []
-    for name, model in models:
-        print('-'*100)
-        kfold = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
-        cv_results = cross_val_score(
-            model, X_train, Y_train, cv=kfold, scoring='accuracy')
-        results.append(cv_results)
-        names.append(name)
-        print('%s: %f (%f)' % (name, cv_results.mean(), cv_results.std()))
+def explain_tree_model(model, X_validation, columns_X):
+    """
+    explain tree models with feature importance and shap values 
+    """
 
-        model.fit(X_train, Y_train)
-        predictions = model.predict(X_validation)
-
-        N = 4
-        print('ACC: ', np.round(accuracy_score(Y_validation, predictions), N))
-        print('ROC: ', np.round(roc_auc_score(Y_validation, predictions), N))
-        print('F1: ', np.round(f1_score(Y_validation, predictions), N))
-        print()
-        print(np.round(confusion_matrix(
-            Y_validation, predictions, normalize='true'), 3))
-        print(confusion_matrix(Y_validation, predictions))
-        print(classification_report(Y_validation, predictions))
-    print('-'*100)
-
-    fi = pd.Series(model.feature_importances_, index=X.columns)
+    fi = pd.Series(model.feature_importances_, index=columns_X)
+    
+    # calculate feature importances
     print(fi.sort_values(ascending=False))
 
-    if False:
-        shap_values = shap.TreeExplainer(model).shap_values(X_train)
-        #shap_values = pd.DataFrame(data=shap_values,columns = data.columns.drop(target))
-        X_train = pd.DataFrame(data=X_train, columns=data.columns.drop(target))
-        #shap.dependence_plot('EQ_DEPTH', shap_values, X_train)
-        shap.summary_plot(shap_values, X_train)
-
+    # calculate shap values
     rf_shap_values = shap.TreeExplainer(model).shap_values(X_validation)
     X_validation = pd.DataFrame(
-        data=X_validation, columns=data.columns.drop(target))
-    # rf_shap_values = pd.DataFrame(data=rf_shap_values,columns = data.columns.drop(target))
+        data=X_validation, columns=columns_X)
+
     shap.summary_plot(rf_shap_values, X_validation)
 
-    print('End')
+
+def run_model_classifier(model, X_train, Y_train, X_validation, Y_validation, results=[], name='model', names=[]):
+    """
+    runs and validates classification models
+    """
+
+    # divide Dataset
+    kfold = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
+    
+    # run kfold model
+    cv_results = cross_val_score(
+        model, X_train, Y_train, cv=kfold, scoring='accuracy')
+    
+    # save results
+    results.append(cv_results)
+    names.append(name)
+    print('%s: %f (%f)' % (name, cv_results.mean(), cv_results.std()))
+
+    # fit model
+    model.fit(X_train, Y_train)
+    predictions = model.predict(X_validation)
+
+    # validate scores
+    N = 4
+    print('ACC: ', np.round(accuracy_score(Y_validation, predictions), N))
+    print('ROC: ', np.round(roc_auc_score(Y_validation, predictions), N))
+    print('F1: ', np.round(f1_score(Y_validation, predictions), N))
+    print()
+    print(np.round(confusion_matrix(
+        Y_validation, predictions, normalize='true'), 3))
+    print(confusion_matrix(Y_validation, predictions))
+    print(classification_report(Y_validation, predictions))
+
+    return model
+
+
+def run_model_regression(model, X_train, Y_train, X_validation, Y_validation, results=[], name='model', names=[]):
+    """
+    runs and validates regression models
+    """
+
+    
+    #divide datasets
+    cv_results = cross_val_score(
+        model, X_train, Y_train, cv=10, scoring='r2')
+    
+    # save results
+    results.append(cv_results)
+    names.append(name)
+    print('%s: %f (%f)' % (name, cv_results.mean(), cv_results.std()))
+
+    # fit model
+    model.fit(X_train, Y_train)
+    predictions = model.predict(X_validation)
+
+    # validate scores
+    N = 4
+    print('max_error: ', np.round(max_error(Y_validation, predictions), N))
+    print('expl_varaiance: ', np.round(
+        explained_variance_score(Y_validation, predictions), N))
+    print('mean_sqr_log: ', np.round(
+        mean_squared_error(Y_validation, predictions), N))
+    print('mean_absolute: ', np.round(
+        mean_absolute_error(Y_validation, predictions), N))
+    print('R2: ', np.round(r2_score(Y_validation, predictions), N))
+
+    return model
--- src/main_function/main_funcion.py
+++ src/main_function/main_funcion.py
@@ -4,7 +4,7 @@ from src.create_model.create_model import create_model
 from src.analyze_data.analyze_data import analyze_data
 
 
-def insurance_weather_model(country):
+def insurance_weather_model(country, bool_log=True, bool_return=True, bool_all_gdp=True, bool_classification=True):
     """
     Funktion liest daten zu einem Land ein
     """
@@ -12,13 +12,14 @@ def insurance_weather_model(country):
     data, geo_data = import_data(country)
 
     # prepare each data individually
-    data = prepare_data_creation(data, bool_return=True, bool_log=False)
+    data = prepare_data_creation(
+        data, bool_return=bool_return, bool_log=bool_log)
 
     # analyze original data
     analyze_data(data)
 
     # merge different datasets
-    data = merge_data(data=data, geo_data=geo_data, bool_all_gdp=True)
+    data = merge_data(data=data, geo_data=geo_data, bool_all_gdp=bool_all_gdp)
 
     # create model
-    create_model(data, 'GDP_Value')
+    create_model(data, 'GDP_Value', bool_classification=bool_classification)
